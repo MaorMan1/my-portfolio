d1063f49b563eaad5c46285537165e465e56c58a
Maor Man <maorman777@gmail.com>
2025-07-22 09:51:20 +0300

    added project

--- backend/index.js
+++ backend/index.js
@@ -20,6 +20,14 @@ app.get('/api/projects', (req, res) => {
     },
     {
       id: 2,
+      name: 'Personal Portfolio Website(this website)',
+      tech: ['React', 'Vite', 'Tailwind CSS', 'TypeScript', 'Node.js'],
+      description: 'A retro pixel-themed personal portfolio website built using React and Tailwind CSS. It includes multiple pages such as Home, Projects, Education, and Experience, with animated components, mobile-ready layout, and integration with a Node.js backend.',
+      year: 2025,
+      github: 'https://github.com/MaorMan1/my-portfolio.git'
+    },
+    {
+      id: 3,
       name: 'Blip&Blop Remake',
       tech: ['C++', 'SFML', 'OOP'],
       description: 'A 2D shooters platform game as a final project for an OOP course, using C++ and the SFML library. The game features players defeating enemies and bosses, avoiding bombs, and collecting weapons while navigating through levels.',
@@ -28,7 +36,7 @@ app.get('/api/projects', (req, res) => {
       youtube: 'https://www.youtube.com/watch?v=sUiOuJve188'
     },
     {
-      id: 3,
+      id: 4,
       name: 'Pacman Remake',
       tech: ['C++', 'SFML', 'OOP'],
       description: 'A Pacman-style game built with C++ and SFML.',
